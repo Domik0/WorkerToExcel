a177ff6dfe92ebe906f4fb1d622e394bc4e5bb28
Rufina Ermeeva <60063672+ermeevar@users.noreply.github.com>
2021-11-29 21:11:18 +0300

    Update Worker.cs

--- WorkerToExel/Worker.cs
+++ WorkerToExel/Worker.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Generic; // Тут вообще не используется этот нэймспэйс
+using System.Linq; // И этот
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks // И этот тоже
 
 namespace WorkerToExel
 {
     public class Worker
     {
-        public byte[] email { get; set; }
-        public byte[] lname { get; set; }
+        public byte[] email { get; set; } // Все свойства должны придерживаться нотации PascalCase, CamelCase подходит только для параметров и переменных
+        public byte[] lname { get; set; } // Сокращение наименований - плохая практика
         public byte[] fname { get; set; }
         public byte[] password { get; set; }
     }
